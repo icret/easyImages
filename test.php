b5fdcf4be3104a42d016c82a9bb0ffd8c79b3843
icret <lemonim@qq.com>
2018-08-06 01:51:59 +0800

    1

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $url = '
-            https://img.545141.com/images/201808/e555eb7fffa9c38c.jpghttps://img.545141.com/images/201808/8c6724af943b8461.jpghttps://img.545141.com/images/201808/70053fd4284d8678.jpg';
-
-    $str = "http://www.jb51.net";
-    $isMatched = preg_match('/[a-zA-z]+://[^\s]* 或 ^http://([\w-]+\.)+[\w-]+(/[\w-./?%&=]*)?$/', $url, $matches);
-    var_dump($isMatched, $matches);
