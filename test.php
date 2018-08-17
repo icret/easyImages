98c78c1506fe58dd894feafff57b0f1fb8053e75
icret <lemonim@qq.com>
2018-08-17 17:54:38 +0800

    fix

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    include 'config.php';
-    $a="https://img.545141.com/crossapi/images/201808/daf111c0b24a5753.png";
-//    $newa = substr($a,-0,strrpos($a,'images/')); //新的$a值
-    $newa = $url = strstr($a,'/'.$config['filePath']);;
-echo $newa;
